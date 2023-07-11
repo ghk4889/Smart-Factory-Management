85d5cffc24155b60ad427af508ac14c53706ca4c
JongDeu <ghk4889@naver.com>
2023-07-11 16:29:28 +0900

    [#1] resolve NoSuchElementException
     두 사람이 동시에 같은 예약을 삭제하려고 할 경우, 늦게 삭제 버튼을 누르는 사람은 이미 제거된 엘리먼트를 조회하게 되므로 NoSuchElementException이 발생한다. 이를 해결했다.
     서버에서 발생한 예외는 @ExceptionHandler로 처리했다. 프론트의 경우, base_paging.html의 ajax(delete) 코드가 서버로부터 error 를 반환 받을 시 error 메시지를 출력하고 페이지를 새로고침을 하여 데이터를 갱신한다.

--- src/main/java/yonam2023/sfproject/logistics/error_and_exception/minusAmountException.java
+++ src/main/java/yonam2023/sfproject/logistics/error_and_exception/minusAmountException.java
@@ -1,4 +1,4 @@
-package yonam2023.sfproject.logistics.exception;
+package yonam2023.sfproject.logistics.error_and_exception;
 
 public class minusAmountException extends RuntimeException{
     public minusAmountException(String message) {
